6bf1ce8c02f9b35f66c24ea2ff0500c00774f076
ekas_7 <ekaspreetatwal@gmail.com>
2025-10-21 13:35:04 +0530

    feat(SOLID): Open/Close Responsibilty

---
+++ 3. Principles/SOLID/OpenClose.java
@@ -0,0 +1,62 @@
+abstract class Shape {
+    abstract double calculateArea();
+}
+
+// Circle class
+class Circle extends Shape {
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Rectangle class
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return width * height;
+    }
+}
+
+// Triangle class
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+class OpenClose {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        Shape triangle = new Triangle(3, 8);
+
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Triangle Area: " + triangle.calculateArea());
+    }
+}
+
