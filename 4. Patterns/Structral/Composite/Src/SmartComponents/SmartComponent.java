6733e48797c9106e020312c83e0ae3dd028f1d35
ekas_7 <ekaspreetatwal@gmail.com>
2025-11-05 02:10:40 +0530

    feat(Structral): Composite

---
+++ 4. Patterns/Structral/Composite/Src/SmartComponents/SmartComponent.java
@@ -0,0 +1,6 @@
+package SmartComponents;
+
+public interface SmartComponent {
+    void turnOn();  // Turn on the component
+    void turnOff(); // Turn off the component
+}
---
+++ 4. Patterns/Structral/Composite/Src/SmartComponents/SmartLight.java
@@ -0,0 +1,12 @@
+package SmartComponents;
+
+public class SmartLight implements SmartComponent {
+  @Override
+  public void turnOn() {
+    System.out.println("Smart Light is now ON.");
+  }
+  @Override
+  public void turnOff() {
+    System.out.println("Smart Light is now OFF.");
+  }
+}
