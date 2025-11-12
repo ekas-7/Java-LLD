e49092d94cb49303ccc79f0acee393625451f7fd
ekas_7 <ekaspreetatwal@gmail.com>
2025-11-12 14:02:42 +0530

    feat(Examples): Tic Tac Toe done

---
+++ 5. LLD Exampes/1. Tic_Tac_Toe/Src/GameStateHandler/ConcreteStates/OTurnState.java
@@ -0,0 +1,22 @@
+package GameStateHandler.ConcreteStates;
+
+import GameStateHandler.Context.GameContext;
+import CommonEnum.Symbol;
+import GameStateHandler.GameState;
+import Utility.Player;
+
+public class OTurnState implements GameState {
+    @Override
+    public void next(GameContext context, Player player, boolean hasWon){
+        if(hasWon){
+            context.setState(player.getSymbol() == Symbol.X ? new XWonState() : new OWonState());
+        }
+        context.setState(new XTurnState());
+    }
+
+    @Override
+    public boolean isGameOver() {
+        return false;
+    }
+    
+}
