c22f48fbd0d573a4b0860ab5b03e4c76e45d823d
ekas_7 <ekaspreetatwal@gmail.com>
2025-12-05 23:40:37 +0530

    feat(examples): Elavator Floor

--- 5. LLD Exampes/4. Elevator/Utility/Floor.java
+++ 5. LLD Exampes/4. Elevator/Utility/Floor.java
@@ -1,5 +1,13 @@
-package Utility;
+package UtilityClasses;
 
 public class Floor {
-    
+    private int floorNumber;
+
+    public Floor(int floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
 }
