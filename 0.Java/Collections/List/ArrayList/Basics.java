a4c2402f00581ac95796c2e39c94368c35632a96
ekas_7 <ekaspreetatwal@gmail.com>
2025-12-11 16:58:32 +0530

    feat(Collections): ArrayList Basics

---
+++ 0.Java/Collections/List/ArrayList/Basics.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Basics {
+    public static void main(String[] args) {
+        // String[] studentName = new String[31];
+
+        // studentName[29] ="yoi";
+        // studentName[30]="yo";
+
+        ArrayList<String> studentName = new ArrayList<>();
+
+        studentName.add("ekas");
+        
+        
+    }
+}
