d35619bbb1adf127afe265ad993cc03db1071a31
ekas_7 <ekaspreetatwal@gmail.com>
2025-12-14 01:46:13 +0530

    feat(Examples): Car Rental System Vehicle

---
+++ 5. LLD Exampes/7. Car_rental_System/VehicleFactory/Vehicle.java
@@ -0,0 +1,11 @@
+package VehicleFactory;
+
+public class Vehicle {
+    private String registrationNumber;
+    private String make;
+    private String model;
+    private int year;
+    private VehicleType type;
+    private VehicleStatus status;
+    private double baseRentalPrice;
+}
